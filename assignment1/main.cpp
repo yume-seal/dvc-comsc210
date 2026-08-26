955df436c931aa9dafc39e846becdd37ccee718d
Madeeha Adam <madeeha@Madeehas-iMac.local>
2026-08-26 11:42:51 -0700

    Defined the functions of MyList

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -18,6 +18,74 @@ class MyList {
         int remove(std::string song);
 };
 
+MyList::MyList(int m) {
+    max = m;
+    size = 0;
+    playlist = new std::string[max];
+}
+
+MyList::~MyList() {
+    delete[]playlist;
+}
+
+bool MyList::isFull() const { 
+    if (size == max) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool MyList::isEmpty() const {
+    if (size == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int MyList::getSize() const {
+    return size;
+}
+
+int MyList::add(std::string song) {
+    if (isFull()) {
+        return 0;
+    }
+    else {
+        playlist[size] = song;
+        size = size + 1;
+        return 1;
+    }
+}
+
+std::string MyList::get(int songNumber) const {
+    if (songNumber < 0 || songNumber >= size) {
+        return "";
+    }
+    else {
+        return playlist[songNumber];
+    }
+}
+
+int MyList::remove(std::string song) {
+    for(int i =0; i < size; i++) {
+        if(playlist[i] == song) {
+            break;
+        }
+        else {
+            return 0;
+        }
+    }
+        for(int j =0; j < size - 2; j++) {
+            playlist[j] = playlist[j + 1];
+        }
+        size = size - 1;
+        return 1;
+    }
+
 int main() {
     return 0;
-}
+};

65c798154bff5d2d4d31b6a6e4905e1c501458e8
Madeeha Adam <madeeha@Madeehas-iMac.local>
2026-09-04 10:18:32 -0700

    Finished MyStack class. Defined constructor, destructor and push method.

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -6,5 +6,31 @@ class MyStack {
     private:
         char* list;
         short int top = -1;
-        short int max;
+        unsigned short int max;
+
+    public: 
+        MyStack(unsigned short s);
+        ~MyStack();
+
+        short push(char*list, short top, unsigned short max, char a);
+        short pop();
+        char peek();
     };
+
+MyStack::MyStack(unsigned short s) {
+    max = s;
+    list = new char[max];
+}
+
+MyStack::~MyStack() {
+    delete[]list;
+}
+
+short MyStack::push(char* list, short top, unsigned short max, char a) {
+    if(top < max - 1) {
+        top = top + 1;
+        list[top] = a;
+        return 0;
+    } else
+        return -1;
+}
